9b69253f16a428dc4bc028851987db98ff8b54ec
Michael K Schumacher <mschu528@gmail.com>
2018-02-28 15:35:46 -0600

    begin adding separate styling for parts pages; use blues instead of greens; begin adding its own nav menu items

--- parts/views.py
+++ parts/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.utils.timezone import now
 
-# Create your views here.
+from . import models as Part
 
 def index(request):
-    return render(request, 'parts/index.html')
+    curr_time = now()
+    recently_added = \
+        Part.Part.objects.all().order_by('-created_at')[:10]
+    recently_updated = \
+        Part.Part.objects.all().order_by('-updated_at')[:10]
+    oos = \
+        Part.Part.objects.filter(in_stock=0).order_by('-updated_at')[:10]
+
+    context = {
+        'curr_time': curr_time,
+        'recently_added': recently_added,
+        'recently_updated': recently_updated,
+        'oos': oos,
+    }
+
+    return render(request, 'parts/index.html', context)
+
+def all(request):
+    return render(request, 'parts/all.html')
