019bc3f4af1da9fd286fc4a507757dc2fe64bc2e
Michael K Schumacher <mschu528@gmail.com>
2018-02-28 20:34:48 -0600

    add view to show individual greensmower data. will use to demo QR code quick access

--- machines/urls.py
+++ machines/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^greensmower/(?P<mower_id>[0-9]+)', views.mower, name="mowerDetail"),
+    url(r'^', views.index, name="index"),
 ]
--- machines/views.py
+++ machines/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.utils.timezone import now
 
-# Create your views here.
+from . import models as Mach
+from maintenance import models as Maint
 
 def index(request):
-    return render(request, 'machines/index.html')
+    curr_time = now()
+
+    context = {
+        'curr_time': curr_time,
+    }
+
+    return render(request, 'machines/index.html', context)
+
+def mower(request, mower_id):
+    curr_time = now()
+
+    mower = Mach.GreensMower.objects.get(pk=mower_id)
+    oil_changes = Maint.OilChange.objects.filter(machine=mower)[:5]
+
+    context = {
+        'curr_time': curr_time,
+        'mower': mower,
+        'oil_changes': oil_changes,
+    }
+
+    return render(request, 'machines/mower.html', context)
