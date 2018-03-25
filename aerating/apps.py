1867dee73a907f951292dc92e26ada4a18f19ff4
Michael K Schumacher <mschu528@gmail.com>
2018-03-25 17:22:48 -0500

    add more test data to DB; create skeleton apps for aerating, building, labor, topdressing, stimping

---
+++ aerating/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AeratingConfig(AppConfig):
+    name = 'aerating'
---
+++ topdressing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TopdressingConfig(AppConfig):
+    name = 'topdressing'
