b7add79d37b31d38cee46f778a9c53492015ac70
Michael K Schumacher <mschu528@gmail.com>
2018-03-30 10:48:58 -0500

    add urls, views, templates for tee rolling

--- rolling/urls.py
+++ rolling/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     
+    url(r'^tees/(?P<pk>\d+)/update/', views.teesUpdate,
+        name="tees_update"),
+    url(r'^tees/(?P<pk>\d+)/edit/', views.teesEdit, name="tees_edit"),
+    url(r'^tees/(?P<pk>\d+)/', views.teesDetail, name="tees_detail"),
+    url(r'^tees/create/', views.teesCreate, name="tees_create"),
+    url(r'^tees/new/', views.teesNew, name="tees_new"),
+    url(r'^tees/', views.teesIndex, name="tees_index"),
+    
     url(r'^greens/(?P<pk>\d+)/update/', views.greensUpdate,
         name="greens_update"),
     url(r'^greens/(?P<pk>\d+)/edit/', views.greensEdit,
--- rolling/views.py
+++ rolling/views.py
@@ -83,3 +83,71 @@ def greensUpdate(request, pk):
             form.save_m2m()
             
     return redirect('roll:greens_detail', pk=rolling.pk)
+
+def teesIndex(request):
+    rollings = TeeRolling.objects.all().order_by('-roll_date')[:20]
+    
+    context = {
+        'curr_time': curr_time(),
+        'rollings': rollings,
+    }
+    
+    return render(request, 'rolling/tees_index.html', context)
+
+def teesNew(request):
+    form = TeeRollingForm()
+    
+    context = {
+        'curr_time': curr_time(),
+        'form': form,
+    }
+    
+    return render(request, 'rolling/tees_new.html', context)
+
+def teesCreate(request):
+    if request.method == 'POST':
+        form = TeeRollingForm(data=request.POST)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+            
+    return redirect('roll:tees_detail', pk=pending_form.pk)
+
+def teesDetail(request, pk):
+    rolling = TeeRolling.objects.get(pk=pk)
+    
+    context = {
+        'curr_time': curr_time(),
+        'rolling': rolling,
+    }
+    
+    return render(request, 'rolling/tees_detail.html', context)
+
+def teesEdit(request, pk):
+    rolling = TeeRolling.objects.get(pk=pk)
+    form = TeeRollingForm(instance=rolling)
+    
+    context = {
+        'curr_time': curr_time(),
+        'rolling': rolling,
+        'form': form,
+    }
+    
+    return render(request, 'rolling/tees_edit.html', context)
+
+def teesUpdate(request, pk):
+    rolling = TeeRolling.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        form = TeeRollingForm(request.POST, instance=rolling)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+            
+    return redirect('roll:tees_detail', pk=rolling.pk)
