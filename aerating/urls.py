3f064ba43f4bba6d7d04307b17768ddd6a7a9496
Michael K Schumacher <mschu528@gmail.com>
2018-04-10 07:26:39 -0500

    finish URLs, views for aerating

--- aerating/urls.py
+++ aerating/urls.py
@@ -4,6 +4,35 @@ from . import views
 
 urlpatterns = [
     
+    url(r'^roughs/(?P<pk>\d+)/update/', views.roughsUpdate,
+        name="roughs_update"),
+    url(r'^roughs/(?P<pk>\d+)/edit/', views.roughsEdit,
+        name="roughs_edit"),
+    url(r'^roughs/(?P<pk>\d+)/', views.roughsDetail, 
+        name="roughs_detail"),
+    url(r'^roughs/create/', views.roughsCreate, name="roughs_create"),
+    url(r'^roughs/new/', views.roughsNew, name="roughs_new"),
+    url(r'^roughs/', views.roughsIndex, name="roughs_index"),
+    
+    url(r'^fairways/(?P<pk>\d+)/update/', views.fairwaysUpdate,
+        name="fairways_update"),
+    url(r'^fairways/(?P<pk>\d+)/edit/', views.fairwaysEdit,
+        name="fairways_edit"),
+    url(r'^fairways/(?P<pk>\d+)/', views.fairwaysDetail,
+        name="fairways_detail"),
+    url(r'^fairways/create/', views.fairwaysCreate, 
+        name="fairways_create"),
+    url(r'^fairways/new/', views.fairwaysNew, name="fairways_new"),
+    url(r'^fairways/', views.fairwaysIndex, name="fairways_index"),
+    
+    url(r'^tees/(?P<pk>\d+)/update/', views.teesUpdate, 
+        name="tees_update"),
+    url(r'^tees/(?P<pk>\d+)/edit/', views.teesEdit, name="tees_edit"),
+    url(r'^tees/(?P<pk>\d+)/', views.teesDetail, name="tees_detail"),
+    url(r'^tees/create/', views.teesCreate, name="tees_create"),
+    url(r'^tees/new/', views.teesNew, name="tees_new"),
+    url(r'^tees/', views.teesIndex, name="tees_index"),
+    
     url(r'^greens/(?P<pk>\d+)/update/', views.greensUpdate,
         name="greens_update"),
     url(r'^greens/(?P<pk>\d+)/edit/', views.greensEdit,
--- aerating/views.py
+++ aerating/views.py
@@ -98,3 +98,208 @@ def greensUpdate(request, pk):
             form.save_m2m()
             
     return redirect('aerate:greens_detail', pk=aerating.pk)
+
+def teesIndex(request):
+    aeratings = TeeAerating.objects.all().order_by(
+        '-aerate_date')[:20]
+    
+    context = {
+        'curr_time': curr_time(),
+        'aeratings': aeratings,
+    }
+    
+    return render(request, 'aerating/tees_index.html', context)
+
+def teesNew(request):
+    form = TeeAeratingForm()
+    
+    context = {
+        'curr_time': curr_time(),
+        'form': form,
+    }
+    
+    return render(request, 'aerating/tees_new.html', context)
+
+def teesCreate(request):
+    if request.method == 'POST':
+        form = TeeAeratingForm(data=request.POST)
+        
+        if form.is_valid() and request.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+
+def teesDetail(request, pk):
+    aerating = TeeAerating.objects.get(pk=pk)
+    
+    context = {
+        'curr_time': curr_time(),
+        'aerating': aerating,
+    }
+    
+    return render(request, 'aerating/tees_detail.html', context)
+
+def teesEdit(request, pk):
+    aerating = TeeAerating.objects.get(pk=pk)
+    form = TeeAeratingForm(instance=aerating)
+    
+    context = {
+        'curr_time': curr_time(),
+        'aerating': aerating,
+        'form': form,
+    }
+    
+    return render(request, 'aerating/tees_edit.html', context)
+
+def teesUpdate(request, pk):
+    aerating = TeeAerating.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        form = TeeAeratingForm(request.POST, instance=aerating)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+            
+    return redirect('aerate:tees_detail', pk=aerating.pk)
+
+def fairwaysIndex(request):
+    aeratings = FairwayAerating.objects.all().order_by(
+        '-aerate_date')[:20]
+    
+    context = {
+        'curr_time': curr_time(),
+        'aeratings': aeratings,
+    }
+    
+    return render(request, 'aerating/fairways_index.html', context)
+
+def fairwaysNew(request):
+    form = FairwayAeratingForm()
+    
+    context = {
+        'curr_time': curr_time(),
+        'form': form,
+    }
+    
+    return render(request, 'aerating/fairways_new.html', context)
+
+def fairwaysCreate(request):
+    if request.method == 'POST':
+        form = FairwayAeratingForm(data=request.POST)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+            
+    return redirect('aerate:fairways_detail', pk=pending_form.pk)
+
+def fairwaysDetail(request, pk):
+    aerating = FairwayAerating.objects.get(pk=pk)
+    
+    context = {
+        'curr_time': curr_time(),
+        'aerating': aerating,
+    }
+    
+    return render(request, 'aerating/fairways_detail.html', context)
+
+def fairwaysEdit(request, pk):
+    aerating = FairwayAerating.objects.get(pk=pk)
+    form = FairwayAeratingForm(instance=aerating)
+    
+    context = {
+        'curr_time': curr_time(),
+        'aerating': aerating,
+        'form': form,
+    }
+    
+    return render(request, 'aerating/fairways_edit.html', context)
+
+def fairwaysUpdate(request, pk):
+    aerating = FairwayAerating.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        form = FairwayAeratingForm(request.POST, instance=aerating)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+            
+    return redirect('aerate:fairways_detail', pk=aerating.pk)
+
+def roughsIndex(request):
+    aeratings = RoughAerating.objects.all().order_by(
+        '-aerate_date')[:20]
+    
+    context = {
+        'curr_time': curr_time(),
+        'aeratings': aeratings,
+    }
+    
+    return render(request, 'aerating/roughs_index.html', context)
+
+def roughsNew(request):
+    form = RoughAeratingForm()
+    
+    context = {
+        'curr_time': curr_time(),
+        'form': form,
+    }
+    
+    return render(request, 'aerating/roughs_new.html', context)
+
+def roughsCreate(request):
+    if request.method == 'POST':
+        form = RoughAeratingForm(data=request.POST)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+    
+    return redirect('aerate:roughs_new', pk=pending_form.pk)
+
+def roughsDetail(request, pk):
+    aerating = RoughAerating.objects.get(pk=pk)
+    
+    context = {
+        'curr_time': curr_time(),
+        'aerating': aerating,
+    }
+    
+    return render(request, 'aerating/roughs_detail.html', context)
+
+def roughsEdit(request, pk):
+    aerating = RoughAerating.objects.get(pk=pk)
+    form = RoughAeratingForm(instance=aerating)
+    
+    context = {
+        'curr_time': curr_time(),
+        'aerating': aerating,
+        'form': form,
+    }
+    
+    return render(request, 'aerating/roughs_edit.html', context)
+
+def roughsUpdate(request, pk):
+    aerating = RoughAerating.objects.get(pk=pk)
+    
+    if request.method == 'POST':
+        form = RoughAeratingForm(request.POST, instance=aerating)
+        
+        if form.is_valid() and request.user.is_authenticated():
+            pending_form = form.save(commit=False)
+            
+            pending_form.save()
+            form.save_m2m()
+            
+    return redirect('aerate:roughs_detail', pk=aerating.pk)
