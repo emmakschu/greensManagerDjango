4d50f9eeeb079ba17483eb9e1bbd3155dd2c1611
Michael K Schumacher <mschu528@gmail.com>
2018-11-02 08:50:12 -0500

    Begin views, templates for labor app

--- labor/views.py
+++ labor/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render, redirect
 from django.utils.timezone import now
 
 from .models import (
-
+    Employee,
+    Task,
+    MowTask,
+    TrapTask,
+    RecordsTask,
+    ShopTask,
+    MiscTask,
+    WtfTask
 )
 
+def curr_time():
+    return now()
 
 def index(request):
-    return render(request, 'labor/index.html')
+    context = {
+        'curr_time': curr_time(),
+    }
+    return render(request, 'labor/index.html', context)
 
-def mowIndex(request):
-    mowing = Mow
+def mowTaskIndex(request):
+    mowTasks = MowTask.objects.all().order_by('started')
+
+    context = {
+        'curr_time': curr_time(),
+        'mow_tasks': mowTasks,
+    }
+
+    return render(request, 'labor/mow_task_index.html', context)
