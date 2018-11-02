282b86ad8fd007b93bb2789ae054c65243f96fd3
Michael K Schumacher <mschu528@gmail.com>
2018-11-02 08:14:00 -0500

    Fix import for User model

--- labor/models.py
+++ labor/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 class Employee(models.Model):
     user = models.ForeignKey(User,
