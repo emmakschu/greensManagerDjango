d1d6c5b00d83377bee343370a20f404be0cdcbc7
Michael K Schumacher <mschu528@gmail.com>
2018-11-02 08:09:01 -0500

    Register Task models with admin site
    Signed-off-by: Michael K Schumacher <mschu528@gmail.com>

--- labor/admin.py
+++ labor/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Employee,
+    Task,
+    MowTask,
+    TrapTask,
+    RecordsTask,
+    ShopTask,
+    MiscTask,
+    WtfTask
+)
+
+admin.site.register(Employee)
+admin.site.register(Task)
+admin.site.register(MowTask)
+admin.site.register(TrapTask)
+admin.site.register(RecordsTask)
+admin.site.register(ShopTask)
+admin.site.register(MiscTask)
+admin.site.register(WtfTask)
