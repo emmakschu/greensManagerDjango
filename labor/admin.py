095d48a9a8b6c35e8f5aacd6cd7e2e333e3ba896
Michael K Schumacher <mschu528@gmail.com>
2019-03-02 08:34:52 -0600

    Clean Task / TaskClass models in admin site to allow user to create their own task classes
    Signed-off-by: Michael K Schumacher <mschu528@gmail.com>

--- labor/admin.py
+++ labor/admin.py
@@ -2,20 +2,9 @@ from django.contrib import admin
 
 from .models import (
     Employee,
-    Task,
-    MowTask,
-    TrapTask,
-    RecordsTask,
-    ShopTask,
-    MiscTask,
-    WtfTask
+    TaskClass,
+    Task
 )
 
 admin.site.register(Employee)
 admin.site.register(Task)
-admin.site.register(MowTask)
-admin.site.register(TrapTask)
-admin.site.register(RecordsTask)
-admin.site.register(ShopTask)
-admin.site.register(MiscTask)
-admin.site.register(WtfTask)
